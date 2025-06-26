daf9ef4b12fdd9ff8f732492e8e40a721367dfbe
Nitish Kumar <kumarnitish4384@gmail.com>
2025-06-26 17:59:55 +0530

    done

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.use('/api/users', followRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
